79dab7fe8c0dc5a9fff5c1de419885c6363be5bf
nut-tr <58322732+nut-tr@users.noreply.github.com>
2019-11-29 15:21:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'สวัสดีครับ มีอะไรให้บีเวอร์ช่วย'
+				/*'text' => $text*/
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
